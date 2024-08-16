f436a64af9a3e5fe340240963e686d41babead46
trung1709 <taductrung1709@gmail.com>
2024-08-17 03:39:48 +0700

    cors

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from "express";
 import 'express-async-errors';
 import { apiRouter } from "./src/api/index.js";
 import { errorHandler } from "./src/error/err-handler.js";
-
-// import cors from 'cors';
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use("/api", apiRouter);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
